fb87c29ba40253adf7bf8e3e66a70eccd37d95ed
eizs798 <eizs798@gmail.com>
2020-02-09 15:09:26 +0800

    Feat | crud page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'note-detail',
+    loadChildren: () => import('./note-detail/note-detail.module').then( m => m.NoteDetailPageModule)
+  },
+  {
+    path: 'note-create',
+    loadChildren: () => import('./note-create/note-create.module').then( m => m.NoteCreatePageModule)
+  },
+  {
+    path: 'note-edit',
+    loadChildren: () => import('./note-edit/note-edit.module').then( m => m.NoteEditPageModule)
+  },
 ];
 
 @NgModule({
